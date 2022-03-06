59827ed4bd98d7426696bf06bb2198a1ca8eb5c6
Juan Jose <jjose.morales@edu.uah.es>
2022-03-06 11:59:41 +0000

    avances en p1.2

--- practica1.2/cliente.py
+++ practica1.2/cliente.py
@@ -2,17 +2,27 @@ import socket
 import sys
 import time
 import select
+import errno
+import threading as th
+
+#def introducir_mensaje():
+ #   return input()
+
+#timeout=4
+#t=th.Timer(timeout,introducir_mensaje)
 
 # Comprobamos numero de argumentos
 n_arg = len(sys.argv)
 if(n_arg!=3):
-    sys.exit('Numero de argumentos erroneo')
+    sys.exit('Numero de argumentos erroneo\n')
 
 print("Buen Dia, introduzca su nombre de usuario\n")
 
 # Nombre de usuario recibido por teclado
+HEADER_LENGTH = 10
 nom_usuario = input()
-
+usuario = nom_usuario.encode("utf-8")
+usuario_header = f"{len(usuario):<{HEADER_LENGTH}}".encode("utf-8")
 # Creamos el socket TCP
 sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -24,19 +34,43 @@ server_address = (ip, puerto)
 
 # Realizamos el connect
 sockfd.connect(server_address)
+sockfd.setblocking(False)
+
+sockfd.send(usuario_header + usuario)
 
 # Empieza el chat
 while True:
-    
-    ready_to_read, ready_to_write, in_error = select.select([sockfd],[sockfd],[],5)
-   
-    if len(ready_to_read) != 0:
-        buf = sockfd.recv(64)  #Recibimos bloques de 64 bytes
-        if len(buf) != 0:
-            print("{}\n".format(buf.decode("utf-8")))
+    msg = input("{}> ".format(nom_usuario))
+    if msg: #Que no este vacio
+       msg = msg.encode("utf-8")
+       msg_header = f"{len(msg):<{HEADER_LENGTH}}".encode("utf-8")
+       sockfd.send(msg_header + msg)
 
-    if len(ready_to_write) != 0:
-        mensaje = input()
-        sockfd.send(mensaje.encode("utf-8"))
+    try:
+        while True:
+            usuario_header = sockfd.recv(HEADER_LENGTH)
+            if not len(usuario_header):
+                print("Cerramos conexion con el servidor")
+                sys.exit()
 
+            usuario_len = int(usuario_header.decode("utf-8").strip())
+            usuario = sockfd.recv(usuario_len).decode("utf-8")
+            
+            msg_header = sockfd.recv(HEADER_LENGTH)
+            msg_len = int(msg_header.decode("utf-8").strip())
+            msg = sockfd.recv(msg_len).decode("utf-8")
 
+            print(f"{usuario} > {msg}")
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print("ERROR")
+            sys.exit()
+
+        continue # SI no es una de esas se contuinua
+
+    except Exception as e:
+        print("ERROR")
+        sys.exit()
+
+       
--- practica1.2/servidor.py
+++ practica1.2/servidor.py
@@ -4,40 +4,56 @@ import sys
 import time
 import select
 
+HEADER_LENGTH = 10
+# Funcion en la que recibimos el mensaje
+def recieve_message(socket_cli):
+    msg_header = socket_cli.recv(HEADER_LENGTH)
+
+    msg_len = int(msg_header.decode("utf-8").strip())
+    return {"header": msg_header, "data": socket_cli.recv(msg_len)}
+
 # Comprobamos numero de argumentos
 n_arg = len(sys.argv)
 if(n_arg!=2):
-    sys.exit('Numero de argumentos erroneo')
+    sys.exit('Numero de argumentos erroneo\n')
 
 
 # Creamos socket y igualamos el puerto a usar
 sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sockfd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1) # Nos permite reconectarnos
 puerto = int(sys.argv[1])
-#bindeamos
+
+# Permitimos la conexion
 server_address = ('', puerto) # '' Significa que es a cualquier direccion
 sockfd.bind(server_address)
-
 sockfd.listen(5)
-lista_sockets = [sockfd]
+
+lista_sockets = [sockfd] #Creamos una lista de sockets para usuarios que se van uniendo
+lista_clientes = {} #Creamos la lista de clientes
 
 while True:
-    ready_to_read, ready_to_write, in_error = select.select(lista_sockets, lista_sockets, [], 5)
-    if len(ready_to_read) != 0:
-        for sock in ready_to_read:
-            if sock is sockfd:
-                conexion_Cliente, dir_Cliente = sock.accept()
-                conexion_Cliente.setblocking(0)
-                lista_sockets.append(conexion_Cliente)
-                print ("Conectado cliente: {}".format(dir_Cliente))
-            else:
-                print("Datos recibidos\n")
-                datos = sock.recv(1024)
-                if not datos:
-                    print("Cliente desconectado")
-                    lista_sockets.remove(sock)
-                else:
-                    print("\nLos datos son: {}".format(datos.decode("utf-8")))
+    ready_to_read, ready_to_write, in_error = select.select(lista_sockets, [], lista_sockets,5)
+    for sock in ready_to_read:
+        if sock is sockfd: #Alguien nuevo se ha conectado al servidor
+            socket_cliente, dir_Cliente = sock.accept()
+            user = recieve_message(socket_cliente)
+            lista_sockets.append(socket_cliente)
+            lista_clientes[socket_cliente]=user
+        else:
+            print("Datos recibidos del cliente:\n")
+                
+            datos = recieve_message(sock)
 
-    if len(ready_to_write) != 0:
-        for sock in ready_to_write:
-            sock.send("Python select server from Ubuntu.\n".encode("utf-8"))
+            if datos is False:
+                print("Cliente desconectado")
+                lista_sockets.remove(sock)
+                del lista_clientes[sock]
+                continue
+            user = lista_clientes[sock]
+
+            for socket_cliente in lista_clientes:
+                if socket_cliente != sock:
+                    socket_cliente.send(user['header'] + user['data'] + datos['header'] + datos['data'])
+
+
+    
